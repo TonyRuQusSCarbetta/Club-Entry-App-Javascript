e2892e036bb262e4150c53406fc685faef9ecc2f
Tony Carbetta <tonycarbetta@gmail.com>
2018-03-28 17:32:25 -0700

    added initial alert, added Media Query to make images display: block on Mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
+alert('You have arrived to Club Carbetta! This is a quick fun app that checks your age to make sure you can get in to the "Club" and decides if you can drink alcohol or not. lol 18+ can get in but can not drink; 21+ can get in and drink; anything under 18 can not get in! lol Try all age groups and see the different results.');
 var firstName = prompt("What is your first name?");
 var lastName = prompt("What's your last name?");
 var age = prompt("What is your age?");
 
-document.getElementById('entrance3').innerHTML = "Welcome to the club " + firstName + " " + lastName + ". ";
+document.getElementById('entrance3').innerHTML = "Hello " + firstName + " " + lastName + ".  Welcome to Club Carbetta.";
 // document.getElementById('entrance3').style.fontSize = 'xx-large';
 
 if (age >= 21) {
@@ -10,8 +11,8 @@ if (age >= 21) {
 
   document.getElementById('entrance').innerHTML = '<img src=\'img/club-get-in.png\'><img src=\'img/free-drink.png\'>';
 
-} else if (age > 18 && age < 21) {
-  document.write("You can't drink any alohol, but you are old enough to get in!");
+} else if (age >= 18 && age < 21) {
+  document.write("You can't drink any alcohol, but you are old enough to get in!");
   document.getElementById('answer').innerHTML = '<img src=\'img/too-young-to-drink.png\'><img src=\'img/club-entrance1.png\'>';
 } else {
   document.write("You are too young to get in!  Don't make me call your parents!");
